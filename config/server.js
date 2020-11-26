94eba7e7ad7a046e95c3435ee193c4e04233106b
Luca <luca.agra@hotmail.com>
2020-11-26 18:11:55 -0300

    Primeiro commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1029088e2bad76ef1f13724ca05d6d85'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '829e7c2e-55c9-4d1b-8f0f-16395941d8bc'
+};
